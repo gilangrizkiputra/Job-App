140f6fa2202f91037d29564899835f94f34e3e57
gilangrizkiputra <gilangrizkiptra@gmail.com>
2025-02-03 15:07:31 +0700

    feat: onboarding and font style poppins

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -5,10 +5,92 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Onboarding Page'),
+    return Scaffold(
+        body: Stack(children: <Widget>[
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/bg.png'), fit: BoxFit.cover),
+        ),
       ),
-    );
+      Padding(
+        padding: const EdgeInsets.only(top: 50, left: 30, right: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Build Your Next \nFuture Career Like \na Master',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              '18,000 jobs available',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontWeight: FontWeight.w300),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 500.0,
+                ),
+                child: Container(
+                  width: 200,
+                  height: 45,
+                  child: TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(66)),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(
+                          color: Color(0xff4141A4),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: Container(
+                  width: 200,
+                  height: 45,
+                  child: OutlinedButton(
+                    style: OutlinedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(66)),
+                      side: BorderSide(
+                        color: Colors.white,
+                        width: 1,
+                      ),
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    ]));
   }
 }
